f72be886fa45abb23d96ac30117e2b8434b98c7b
ygidtu <ygidtu@gmail.com>
2022-04-08 10:58:25 +0800

    better ETA

--- progress.go
+++ progress.go
@@ -9,11 +9,11 @@ func BytesBar(size int64, name string) *mpb.Bar {
 	return p.AddBar(size,
 		mpb.PrependDecorators(
 			decor.Name(name, decor.WC{W: len(name) + 1, C: decor.DidentRight}),
-			decor.CountersKibiByte("% .2f / % .2f"),
+			decor.CountersKibiByte("[% .2f / % .2f]"),
 		),
 		mpb.AppendDecorators(
-			decor.EwmaETA(decor.ET_STYLE_GO, 90),
-			decor.Name(" ] "),
+			decor.AverageETA(decor.ET_STYLE_GO),
+			decor.Name(" | "),
 			decor.AverageSpeed(decor.UnitKiB, "% .2f"),
 		),
 	)
